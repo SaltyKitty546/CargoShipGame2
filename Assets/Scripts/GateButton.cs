ba520a67c81bd6d078500821d6facbe32a28ae27
SaltyKitty546 <anders@pjacobsson.com>
2025-02-22 11:54:48 -0800

    massive news for the developer

---
+++ Assets/Scripts/GateButton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gate;
+    public Inventory i;
+    public bool isOpen = false;
+    //public AudioSource buttonPress;
+    //public AudioSource gateOpen;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen) {
+            gate.SetActive(false);
+        } else {
+            gate.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown("e")) {
+            if (i.haveRedKeycard && i.haveBlueKeycard && i.haveGreenKeycard) {
+                isOpen = true;
+                i.haveBlueKeycard = false;
+                i.haveGreenKeycard = false;
+                i.haveRedKeycard = false;
+                //buttonPress.Play();
+                //gateOpen.Play();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Sounds-Music/MuiscManagement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuiscManagement : MonoBehaviour
+{
+    
+    public AudioSource track1;
+    public AudioSource track2;
+    public AudioSource track3;
+    public AudioSource track4;
+    public AudioSource track5;
+    public AudioSource track6;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void TrackChecker()
+    {
+        
+    }
+}
