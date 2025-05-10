9e5538a1b02de2c3eef1870f7550584c0b13287d
SaltyKitty546 <anders@pjacobsson.com>
2025-05-10 11:55:26 -0700

    balls

--- Assets/Scripts/Sounds-Music/MuiscManagement.cs
+++ Assets/Scripts/Sounds-Music/MuiscManagement.cs
@@ -7,45 +7,29 @@ public class MuiscManagement : MonoBehaviour
     
     public AudioSource track1;
     public AudioSource track2;
-    public AudioSource track3;
-
-    public int musicCooldown = 0;
     public int chosenTrack;
     
 
     void Start()
     {
-        
+        ChooseTrack();
     }
 
     // Update is called once per frame
     void Update()
     {
-        musicCooldown -= 1;
-        if (musicCooldown < 0)
-        {
-            ChooseTrack();
+             
+    }
+
+    public void ChooseTrack() {
+        chosenTrack = Random.Range(0, 3);
+        if (chosenTrack == 1) {
+            track1.Play();
+        } else {
+            track2.Play();
         }
+        Invoke("ChooseTrack", 245f);
         
     }
 
-    public void ChooseTrack()
-    {
-        chosenTrack = Random.Range(1, 3);
-        musicCooldown = 17000;
-        track1.Stop();
-        track2.Stop();
-        track3.Stop();
-
-        if (chosenTrack == 1)
-        {
-            track1.Play();
-        } else if (chosenTrack == 2)
-        {
-            track2.Play();
-        } else if (chosenTrack == 3)
-        {
-            track3.Play();
-        }
-    }
 }
